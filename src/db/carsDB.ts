4fb2bb8f994dc7229771031b9853b6a9f4eb839e
fox1s <ostinsmikh@gmail.com>
2021-01-20 20:48:38 +0200

    first commit

---
+++ src/db/carsDB.ts
@@ -0,0 +1,44 @@
+export let teslaCars = [
+  {
+    model: '3',
+    manufactureOfCar: 2018,
+    power: '210 kwt',
+    color: 'grey',
+    driver: {name: 'wfwf', age: '56', gen: 'male', practice: 19}
+  },
+  {
+    model: 'X',
+    manufactureOfCar: 2018,
+    power: '200 kwt',
+    color: 'red',
+    driver: {name: 'ybby', age: '12', gen: 'female', practice: 25}
+  },
+  {
+    model: 'Y',
+    manufactureOfCar: 2021,
+    power: '240 kwt',
+    color: 'black',
+    driver: {name: 'untnt', age: '65', gen: 'male', practice: 60}
+  },
+  {
+    model: 'S',
+    manufactureOfCar: 2019,
+    power: '180 kwt',
+    color: 'pink',
+    driver: {name: 'wvvtrewbrt', age: '78', gen: 'female', practice: 50}
+  },
+  {
+    model: 'Cybertrack',
+    manufactureOfCar: 2022,
+    power: '220 kwt',
+    color: 'grey',
+    driver: {name: 'dzfbdzbd', age: '56', gen: 'female', practice: 30}
+  },
+  {
+    model: 'roadster',
+    manufactureOfCar: 2023,
+    power: '500 kwt',
+    color: 'red',
+    driver: {name: 'mol', age: '25', gen: 'male', practice: 15}
+  },
+];
