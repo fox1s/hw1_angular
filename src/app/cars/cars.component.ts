abf888a311097d33d88108829357bf6093e1fc19
fox1s <ostinsmikh@gmail.com>
2021-01-20 20:50:07 +0200

    sec commit

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {teslaCars} from '../../db/carsDB';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+
+  cars = teslaCars;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
